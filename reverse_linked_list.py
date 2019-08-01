84686471b2e5e0ac8173e5eb393c90a918c9411d
smona <silbertmonaphia@gmail.com>
2019-08-01 22:12:50 +0800

    feat: solve leetcode 206. Reverse Linked List

---
+++ reverse_linked_list.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def traverse(self):
+        node = self
+        while node != None:
+            print(node.val)
+            node = node.next
+
+class SolutionIterative:
+    def reverseList(self, head: Node) -> Node:
+        cur, prev = head, None
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
+
+class SolutionRecursive:
+    def reverseList(self, head: Node, prev=None) -> Node:
+        if not head:
+            return prev
+        cur, head.next = head.next, prev
+        return self.reverseList(cur, head)
+
+if __name__ == '__main__':
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n1.next = n2
+    n2.next = n3
+
+    from copy import deepcopy
+    head = deepcopy(n1)
+    new_head = SolutionIterative().reverseList(head)
+    new_head.traverse()
+
+    head = deepcopy(n1)
+    new_head = SolutionRecursive().reverseList(n1)
+    new_head.traverse()
