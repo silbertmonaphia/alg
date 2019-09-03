8f8f6b1e5ab948005dbec103ef7777a14a2fc236
smona <silbertmonaphia@gmail.com>
2019-09-03 21:36:16 +0800

    feat: add 20_ValidParentheses.py leetcode

---
+++ 20_ValidParentheses.py
@@ -0,0 +1,26 @@
+from data_struture import Stack
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = Stack()
+        lookup = {')': '(', ']':'[', '}': '{'}
+        for c in s:
+            if c not in lookup:
+                stack.push(c)
+            elif stack.is_empty() or stack.pop() != lookup[c]:
+                return False
+        return stack.is_empty()
+
+if __name__ == '__main__':
+    tests = ["(){}[]","([{]})","(]","([)]","())","(()"]
+    results = []
+    for test in tests:
+        result = Solution().isValid(test)
+        print(result)
+        results.append(result)
+    assert(results == [True,False,False,False,False,False])
+        
