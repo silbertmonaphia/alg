5a35acf23667a13699f07a34c6cb569d5c1f9c67
smona <silbertmonaphia@gmail.com>
2019-12-10 23:16:42 +0800

    fix: refine 15_3Sum.py

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -1,22 +1,47 @@
 class Solution:
     def threeSum(self, nums):
+        # This impl is neat!O(n2)
         if len(nums) < 3:
             return []
         nums.sort()
         res = set()
-        for i,v in enumerate(nums[:-2]):
-            if i >= 1 and v == nums[i-1]:
-                # 如果和前面一个值相等就跳过，因为情况和前面一个值都是一样的
+        for i, v in enumerate(nums[:-2]):
+            if i >= 1 and v == nums[i - 1]:
                 continue
-            d = {}
-            for x in nums[i+1:]:
+            d = {} # Extra space for a dict O(n)
+            for x in nums[i + 1:]:
                 if x not in d:
-                    d[-v-x] = 1
+                    d[-v - x] = 1
                 else:
-                    res.add((v,-v-x,x))
+                    res.add((v, -v - x, x))
         return list(res)
-                
 
-test_case = [-1,0,1,2,-1,-4]
+    def threeSum_n2(self, nums):
+        # O(n2)
+        res = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                # Search from both ends to the middle
+                s = nums[i] + nums[left] + nums[right]
+                if s < 0:
+                    left += 1
+                elif s > 0:
+                    right -= 1
+                else:
+                    res.append((nums[i], nums[left], nums[right]))
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left > right and nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+
+
+test_case = [-1, 0, 1, 2, -1, -4]
 res = Solution().threeSum(test_case)
 assert(res == [(-1, -1, 2), (-1, 0, 1)])
+for result in res:
+    assert(sum(result) == 0)
