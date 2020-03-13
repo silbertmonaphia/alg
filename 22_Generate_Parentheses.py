5d9edd851eaeb143496ca8975cdeeefe0b396f53
smona <silbertmonaphia@gmail.com>
2020-03-13 22:59:07 +0800

    feat: add 22_Generate_Parentheses.py

---
+++ 22_Generate_Parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n):
+        self.list = []
+        self._gen(0, 0, n, "")
+        return self.list
+
+    def _gen(self, left, right, n, result):
+        if left == n and right == n:
+            self.list.append(result)
+            return
+        if left < n:
+            self._gen(left+1, right, n, result+'(')
+        if right < left:
+            self._gen(left, right+1, n, result+')')
+
+
+print(Solution().generateParenthesis(3))
+assert(Solution().generateParenthesis(3) == [
+       '((()))', '(()())', '(())()', '()(())', '()()()'])
